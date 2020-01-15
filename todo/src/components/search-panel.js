5f73e438c229c2a5714580c0ce42557f80ec9f09
pauers <korenev@dev.picom.ru>
2020-01-15 12:34:05 +0400

    028 make folder for components

---
+++ todo/src/components/search-panel.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchPanel = () => {
+  const searchText = "Type here to Search";
+  const searchStyle = {
+    fontSize: "20px"
+  };
+  return <input placeholder={searchText} style={searchStyle} />;
+};
+
+export default SearchPanel;
