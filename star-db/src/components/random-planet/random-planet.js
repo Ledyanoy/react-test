250d3b4b170a3d5b503583c2d724bfe1643af8d8
pauers <korenev@dev.picom.ru>
2020-02-11 13:27:20 +0400

    057

--- star-db/src/components/random-planet/random-planet.js
+++ star-db/src/components/random-planet/random-planet.js
@@ -5,11 +5,7 @@ export default class RandomPlanet extends Component {
   swapiService = new SwapiService();
 
   state = {
-    id: null,
-    name: null,
-    population: null,
-    rotationPeriod: null,
-    diameter: null
+    planet: {}
   };
 
   constructor() {
@@ -17,21 +13,19 @@ export default class RandomPlanet extends Component {
     this.updatePlanet();
   }
 
+  onPlanetLoadead = planet => {
+    this.setState({ planet });
+  };
+
   updatePlanet() {
     const id = Math.floor(Math.random() * 25) + 2;
-    this.swapiService.getPlanet(id).then(planet => {
-      this.setState({
-        id,
-        name: planet.name,
-        population: planet.population,
-        rotationPeriod: planet.rotation_period,
-        diameter: planet.diameter
-      });
-    });
+    this.swapiService.getPlanet(id).then(this.onPlanetLoadead);
   }
 
   render() {
-    const { id, name, population, rotationPeriod, diameter } = this.state;
+    const {
+      planet: { id, name, population, rotationPeriod, diameter }
+    } = this.state;
     return (
       <div className="random-planet jumbotron rounded">
         <img
