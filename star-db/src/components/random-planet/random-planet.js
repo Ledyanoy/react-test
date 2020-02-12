83227c72380464b25952915403c8bd395089fbf5
Ledaynoy <ledaynoy44@hmail.com>
2020-02-12 15:24:17 +0400

    060

--- star-db/src/components/random-planet/random-planet.js
+++ star-db/src/components/random-planet/random-planet.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "./random-planet.css";
 import SwapiService from "../../services/swapi-service";
+import Spinner from '../spinner';
+import ErrorIndicator from '../error-indicator';
 export default class RandomPlanet extends Component {
   swapiService = new SwapiService();
 
   state = {
-    planet: {}
+    planet: {},
+    loading: true
   };
 
   constructor() {
@@ -14,21 +17,44 @@ export default class RandomPlanet extends Component {
   }
 
   onPlanetLoadead = planet => {
-    this.setState({ planet });
+    this.setState({ planet, loading: false, error: false });
   };
+
+  onError=(err)=> {
+    this.setState({ loading: false, error: true });
+
+  }
 
   updatePlanet() {
     const id = Math.floor(Math.random() * 25) + 2;
-    this.swapiService.getPlanet(id).then(this.onPlanetLoadead);
+    this.swapiService.getPlanet(id).then(this.onPlanetLoadead).catch(this.onError)
   }
 
   render() {
-    const {
-      planet: { id, name, population, rotationPeriod, diameter }
-    } = this.state;
+    // return <Spinner/>;
+    const { planet,loading, error } = this.state;
+
+    const hasData = !(loading || error);
+    const errorMassage = error ? <ErrorIndicator/> : null;
+    const spinner = loading ? <Spinner/> : null;
+    const content = !hasData ? null : <PlanetView planet={planet}/>;
     return (
       <div className="random-planet jumbotron rounded">
-        <img
+        {errorMassage}
+        {spinner}
+        {content}       
+        
+      </div>
+    );
+  }
+}
+
+const PlanetView =({planet}) => {
+  const  { id, name, population, rotationPeriod, diameter } = planet;
+  
+  return (
+    <React.Fragment>
+      <img
           className="planet-image"
           src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
         />
@@ -49,7 +75,6 @@ export default class RandomPlanet extends Component {
             </li>
           </ul>
         </div>
-      </div>
-    );
-  }
-}
+    </React.Fragment>
+  );
+};
